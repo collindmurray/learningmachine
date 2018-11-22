dc7f1b714ef245e6fec124fe7e61be400abc23e5
Collin Murray <collin.d.murray@gmail.com>
2018-11-21 19:59:54 -0600

    beatified learn.py

--- working/learn.py
+++ working/learn.py
@@ -1,7 +1,8 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 import pandas as pd
-# import seaborn as sns
+# import seabornas sns
 import sklearn
-
 
 data = pd.read_csv("resources/primary_walk_vs_phones.csv")
 
@@ -10,28 +11,21 @@ data = pd.read_csv("resources/primary_walk_vs_phones.csv")
 # print(data.tail(10))
 # print(data.shape)
 
-
-# sns.pairplot(data, x_vars = ['walkcontacts', 'phonecontacts'], y_vars = ['voted'])
+# sns.pairplot(data, x_vars=['walkcontacts', 'phonecontacts'], y_vars=['voted'])
 
 X = data[['walkcontacts', 'phonecontacts']]
 Y = data[['voted']]
 
 # print(X.head(5))
 
-
-
-from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state = 1)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=1)
 print(X_train.shape)
 print(X_test.shape)
 print(Y_train.shape)
 print(Y_test.shape)
-
-from sklearn.linear_model import LinearRegression
 
 linreg = LinearRegression()
 linreg.fit(X_train, Y_train)
 
 print(linreg.intercept_)
 print(linreg.coef_)
-
